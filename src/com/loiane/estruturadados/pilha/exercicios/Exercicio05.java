9114d6fedc0f1850a671b115f9ef259bfa94f164
eslinmakicine <eslinmartins@gmail.com>
2022-02-07 22:06:31 -0300

    inserindo exercicios de pilha

--- src/com/loiane/estruturadados/pilha/exercicios/Exercicio05.java
+++ src/com/loiane/estruturadados/pilha/exercicios/Exercicio05.java
@@ -1,10 +1,42 @@
 package com.loiane.estruturadados.pilha.exercicios;
 
+import com.loiane.estruturadados.pilha.Pilha;
+
 public class Exercicio05 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		imprimeResultado("ADA");
+
+		imprimeResultado("ABCD");
+
+		imprimeResultado("ABCCBA");
+
+		imprimeResultado("Maria");
 	}
 
+	public static void imprimeResultado(String palavra){
+		System.out.println(palavra + " é palindromo? " + testaPalindromo(palavra));
+	}
+
+	public static boolean testaPalindromo(String palavra){
+
+		Pilha<Character> pilha = new Pilha<Character>(); 
+
+		for (int i=0; i<palavra.length(); i++){
+			pilha.empilha(palavra.charAt(i));
+		}
+
+		String palavraInversa = "";
+		while (!pilha.estaVazio()){
+			palavraInversa += pilha.desempilha();
+		}
+
+		if (palavraInversa.equalsIgnoreCase(palavra)){
+			return true;
+		}
+
+		return false;
+	}
 }
+
